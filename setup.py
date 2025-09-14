23c42fad85d9d0d63c37c9191847657e2805cab5
AkshayK2026 <akshaykadam.7096@gmail.com>
2025-09-14 13:48:37 +0530

    requirements are added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Akshay kadam",
+    author_email="akshaykadam.7096@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
